b373c932dbdcbaffc24790af2fe813ab7051c0a0
Mark Daniel Louwe <mrkdnlph@gmail.com>
2019-08-22 13:34:09 +0800

    first commit

---
+++ member-dir-listing.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: CLL Member Directory Listing
+Plugin URI:  tba 
+Description: Creates an edit User Profile link/page
+Version:     1.0.0
+Author:      Mark Daniel Louwe, Danica Niña Louwe
+Author URI:  http://creativelabbylouwe.com
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+*/
